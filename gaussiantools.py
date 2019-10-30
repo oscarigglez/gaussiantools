aad92dd03718725d3a4abc160720fcfdbddb8431
Óscar <oscarigglez@gmail.com>
2019-10-30 09:21:17 +0100

    Update gaussiantools.py

--- gaussiantools.py
+++ gaussiantools.py
@@ -1,3 +1,11 @@
-#!/anaconda3/bin/python
+element_dict = {}
 
+class Structure:
+    def __init__(self, *args, **kwargs):
+        self.name = kwargs.get('name', "system")
+        print("Molecule instantiated!\n")
 
+def read_xyz(xyz):
+    with open(xyz, 'r') as open_xyz:
+        xyz_lines = open_xyz.readlines()[2:]
+    return
